c8f79cbfe4c7dff7ecdadce71b4ce9634af29404
Chr8s0 <kr8szt8an.s8pos@gmail.com>
2025-02-04 08:45:27 +0100

    Árnyék

---
+++ frontend/src/components/Szuro.jsx
@@ -0,0 +1,35 @@
+function Szuro() {
+    return (
+        <div className="tab-content">
+            <div id="szemelyauto" className="kereso tab-pane active">
+                <div className="micro-frontend-component" data-mfc-component-name="PersonalVehicleSearchBox" data-mfc-props='null'>
+                    <div className="skeleton-container">
+                        <div className="upper-section">
+                            <div className="upper-section__input-field upper-section__input-field--brand"></div>
+                            <div className="upper-section__input-field upper-section__input-field--model"></div>
+                            <div className="upper-section__input-field upper-section__input-field--variant"></div>
+                            <div className="upper-section__input-field upper-section__input-field--fuel"></div>
+                            <div className="upper-section__input-field upper-section__input-field--year"></div>
+                            <div className="upper-section__input-field upper-section__input-field--year"></div>
+                            <div className="upper-section__input-field upper-section__input-field--price"></div>
+                            <div className="upper-section__input-field upper-section__input-field--price"></div>
+                        </div>
+                        <div className="lower-section">
+                            <div className="checkbox-container">
+                                <div className="checkbox-container__checkbox"></div>
+                                <div className="checkbox-container__checkbox"></div>
+                                <div className="checkbox-container__checkbox"></div>
+                            </div>
+                            <div className="search-button-container">
+                                <div className="search-button-container__search-button search-button-container__search-button--detailed"></div>
+                                <div className="search-button-container__search-button search-button-container__search-button--simple"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Szuro;
