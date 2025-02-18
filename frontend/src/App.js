7def1b3d1e0a08e9c5944df00300418dbd09dde3
Chr8s0 <kr8szt8an.s8pos@gmail.com>
2025-02-18 11:16:09 +0100

    Menüsor működése

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -56,57 +56,6 @@ function App() {
     <div className="szin" style={appStyle}>
       <Menu />
 
-      {/* Videó háttér */}
-      <div className="video-hatter">
-        <Video />
-
-        <Container className="my-4" style={{ position: 'relative', zIndex: 1 }}>
-          <Row className="d-flex justify-content-between kinalat">
-            {/* Szűrő oldalsáv */}
-            <Col xs={12} sm={3} md={3} lg={2}>
-              <Szuro onFilterChange={handleFilterChange} products={products} />
-            </Col>
-
-            {/* Termékek megjelenítése */}
-            <Col xs={12} sm={9} md={9} lg={10} className="card-container">
-              {filteredProducts.length > 0 && (
-                <>
-                  <h1 className="text-center mb-4">Jelenlegi kínálatunk</h1>
-                  <Row className="d-flex justify-content-start g-4">
-                    {filteredProducts.map((auto) => (
-                      <Col
-                        key={auto.Rendszam}
-                        xs={12} sm={6} md={4} lg={4}
-                        style={{ padding: '10px', maxWidth: '350px' }}
-                      >
-                        <CustomCard
-                          imageSrc={`http://localhost:8080/${auto.Modell}.jpg`}
-                          title={`${auto.Marka} ${auto.Modell}`}
-                          subtitle={`Évjárat: ${auto.Evjarat} | Ár: ${auto.Ar} Ft`}
-                          description={`Kilométeróra: ${auto.Kilometerora} | Üzemanyag: ${auto.Motortipus}`}
-                          adatok={`Km.állás: ${auto.Kilometerora} | Motortípus: ${auto.Motortipus} | Motorspec.: ${auto.Motorspecifikacio} | Sebességváltó: ${auto.Sebessegvalto} | Használat: ${auto.Hasznalat} | Autó színe: ${auto.Szin}`}
-                          year={`${auto.Rendszam}`}
-                          elado={`${auto.Nev} | Tel.: ${auto.Telefon} | Email: ${auto.Email}`}
-                          isFavorite={favorites.includes(auto.Rendszam)}
-                          onFavoriteToggle={() => handleFavoriteToggle(auto.Rendszam)}
-                        />
-                      </Col>
-                    ))}
-                  </Row>
-                </>
-              )}
-
-              {filteredProducts.length === 0 && (
-                <p className="text-center">Nincs megjeleníthető autó.</p>
-              )}
-
-              {error && <p className="text-danger text-center">{error}</p>}
-            </Col>
-          </Row>
-        </Container>
-
-      </div>
-
       <Footer />
     </div>
   );
