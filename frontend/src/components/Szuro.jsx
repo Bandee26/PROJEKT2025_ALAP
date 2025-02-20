87e10653e5346266dec8b095e744d51d8266f0c2
Chr8s0 <kr8szt8an.s8pos@gmail.com>
2025-02-20 11:41:07 +0100

    Szűrő kocsi ikonnal

--- frontend/src/components/Szuro.jsx
+++ frontend/src/components/Szuro.jsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 import ReactSlider from 'react-slider';
 import axios from 'axios';
 import { Card, Form, Row, Col } from 'react-bootstrap';
+import carIcon from './auto.png';
 import './Szuro.css'; // Egyedi stílusokhoz
+
 
 const Szuro = ({ onFilterChange, products }) => {
   const [brands, setBrands] = useState([]);
@@ -78,13 +80,19 @@ const Szuro = ({ onFilterChange, products }) => {
             value={priceRange}
             onChange={setPriceRange}
             className="custom-slider"
-            thumbClassName="slider-thumb"
+            renderThumb={(props, state) => (
+              <div {...props}>
+                <img src={carIcon} alt="car" className="slider-car" />
+              </div>
+            )}
             trackClassName="slider-track"
           />
+
           <Row className="mt-2">
-            <Col><small className="text-light">Min: {priceRange[0]} Ft</small></Col>
-            <Col className="text-end"><small className="text-light">Max: {priceRange[1]} Ft</small></Col>
+            <Col><small className="text-light">{priceRange[0]} Ft</small></Col>
+            <Col className="text-end"><small className="text-light">{priceRange[1]} Ft</small></Col>
           </Row>
+
         </Form.Group>
     </Card>
   );
