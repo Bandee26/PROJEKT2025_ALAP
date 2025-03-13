4fc8ae8a7925c0a9d7d919541c2ee789b246c854
Chr8s0 <kr8szt8an.s8pos@gmail.com>
2025-03-13 11:43:13 +0100

    25-sével tölti be a kártyákat

--- backend/routes/termek.js
+++ backend/routes/termek.js
@@ -5,9 +5,10 @@ var router = express.Router();
 var Db = require('../db/dboperations');
 
 /* GET autorendszer */
-router.get('/', async function(req, res, next) {
+router.get('/:pageNo', async function(req, res, next) {
   try {
-    const products = await Db.selectAutoFromAutorendszer();  // Lekérdezés az autorendszer nézetből
+    let oldal = Number(req.params.pageNo)
+    const products = await Db.selectAutoFromAutorendszer(oldal);  // Lekérdezés az autorendszer nézetből
     res.json({ products: products });  // JSON válasz
   } catch (error) {
     res.status(500).send('Szerver hiba!'); // Hibaüzenet visszaküldése
